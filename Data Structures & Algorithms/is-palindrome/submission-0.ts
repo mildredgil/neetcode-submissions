c0c1becf294ff3b6d854c3d4510ce306670f50bd
mildredgil <mi.mitel@hotmail.com>
2026-09-02 20:54:47 -0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        const cleanS = s.replace(/[^a-zA-Z0-9]/g, "");
+        const lowerS = cleanS.toLowerCase()
+        console.log(lowerS)
+
+        for(let i = 0, j = lowerS.length - 1; i <= j; i++, j--) {
+            if(lowerS[i] !== lowerS[j]) return false
+        }
+
+        return true
+    }
+}
